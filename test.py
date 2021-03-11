be2c8c14e4abd5960bf37af7ccc50fcdd0605b3f
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-03-11 13:08:45 +0000

    Update

---
+++ test.py
@@ -0,0 +1,25 @@
+correct_answer = "Correto!"
+wrong_answer = "Errado!"
+a = 100
+b = -100
+total = a + b
+if total == 0:
+    print(correct_answer)
+else:
+        print(wrong_answer)
+
+d = 20
+s = 20
+total1 = d + s
+if total1 == 0:
+    print(correct_answer)
+else:
+    print(wrong_answer)
+
+    g = 10
+    j = 20000
+    total2 = g + j
+if total2 == 1000:
+        print(correct_answer)
+else:
+    print(wrong_answer)
