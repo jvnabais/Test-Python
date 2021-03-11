215c182f6800a5fad3f606cf66a71833aebf0079
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-03-11 15:07:54 +0000

    Big Update

--- test.py
+++ test.py
@@ -9,24 +9,39 @@ else:
         print(wrong_answer)
 
 d = 20
-s = 20
+s = -20
 total1 = d + s
 if total1 == 0:
     print(correct_answer)
 else:
     print(wrong_answer)
 
-    g = 10
-    j = 20000
-    total2 = g + j
+g = 10
+j = 20000
+total2 = g + j
 if total2 == 1000:
         print(correct_answer)
 else:
     print(wrong_answer)
     n = 50
-    m = -50
+    m = 50
     total3 = n + m 
 if total3 == 0:
+    print(correct_answer)
+else:
+    print(wrong_answer)
+
+y = 80
+z = -80
+total4 = y + z
+if total4 == 0:
+        print(correct_answer)
+else:
+        print(wrong_answer)
+o = 20
+p = -20
+total5 = o + p
+if total5 == 0:
     print(correct_answer)
 else:
     print(wrong_answer)
