6f9de7d401c083ce040fdf2954a18d930902454d
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-03-12 23:28:02 +0000

    Update

--- test.py
+++ test.py
@@ -1,34 +1,42 @@
-print("Esta pergunta é sobre um animal...")
+score = 0 
+print("Qual é o cão mais simpático do mundo?")
 input1 = input("Input ")
 if input1 == "Freddie":
     print("Correto!")
-    print("Está pergunta é em inglés")
+    print("3 palavras muito usadas por nós em inglés")
+    score = score+1
 else:
     print("Errado!")
-    print("Está pergunta é em inglés")
+    print("3 palavras muito usadas por nós em inglés")
 input2 = input("Input ")
-if input2 == "Love you":
+if input2 == "I Love You":
     print("Correto!")
-    print("Esta pergunta é sobre uma pessoa nesta casa(mulher)")
+    print("Uma mulher nesta casa")
+    score = score+1
 else:
     print("Errado!")
-    print("Esta pergunta é sobre uma pessoa nesta casa(mulher)")
+    print("Uma mulher nesta casa")
 input3 = input("Input ")
-if input3 == "Mãe":
-    print("Certo!")
-    print("Esta pergunta é sobre uma pessoa nesta casa(homem)")
+if input3 == "Joana":
+    print("Correto!")
+    print("Um homem nesta casa")
+    score = score+1
 else:
     print("Errado!")
-    print("Esta pergunta é sobre uma pessoa nesta casa(homem)")
+    print("Um homem nesta casa")
 input4 = input("Input ")
-if input4 == "Pai":
-    print("Certo!")
+if input4 == "Carlos":
+    print("Correto!")
     print("O nome de uma pessoa muito amada")
+    score = score+1
 else:
     print("Errado!")
     print("O nome de uma pessoa muito amada")
 input5 = input("Input ")
-if input5 == "Joana":
+if input5 == "João":
     print("Correto!")
+    score = score+1
+    print("Your total score is ",score,"out of 10")
 else:
     print("Errado!")
+    print("Your total score is ",score,"out of 5")
