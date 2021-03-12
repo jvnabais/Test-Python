c4e78311976091e03bb3811d8c00d899e156e4f1
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-03-12 22:55:36 +0000

    New python file

---
+++ test.py
@@ -0,0 +1,34 @@
+print("Esta pergunta é sobre um animal...")
+input1 = input("Input ")
+if input1 == "Freddie":
+    print("Correto!")
+    print("Está pergunta é em inglés")
+else:
+    print("Errado!")
+    print("Está pergunta é em inglés")
+input2 = input("Input ")
+if input2 == "Love you":
+    print("Correto!")
+    print("Esta pergunta é sobre uma pessoa nesta casa(mulher)")
+else:
+    print("Errado!")
+    print("Esta pergunta é sobre uma pessoa nesta casa(mulher)")
+input3 = input("Input ")
+if input3 == "Mãe":
+    print("Certo!")
+    print("Esta pergunta é sobre uma pessoa nesta casa(homem)")
+else:
+    print("Errado!")
+    print("Esta pergunta é sobre uma pessoa nesta casa(homem)")
+input4 = input("Input ")
+if input4 == "Pai":
+    print("Certo!")
+    print("O nome de uma pessoa muito amada")
+else:
+    print("Errado!")
+    print("O nome de uma pessoa muito amada")
+input5 = input("Input ")
+if input5 == "Joana":
+    print("Correto!")
+else:
+    print("Errado!")
