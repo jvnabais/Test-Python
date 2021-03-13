9c5f0987df0d3b94066ad6906de80ddaf7b4989c
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-03-13 10:55:17 +0000

    Update

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 score = 0 
 print("Qual é o cão mais simpático do mundo?")
-input1 = input("Input ")
+input1 = input("Resposta:")
 if input1 == "Freddie":
     print("Correto!")
     print("3 palavras muito usadas por nós em inglés")
@@ -8,7 +8,7 @@ if input1 == "Freddie":
 else:
     print("Errado!")
     print("3 palavras muito usadas por nós em inglés")
-input2 = input("Input ")
+input2 = input("Resposta:")
 if input2 == "I Love You":
     print("Correto!")
     print("Uma mulher nesta casa")
@@ -16,7 +16,7 @@ if input2 == "I Love You":
 else:
     print("Errado!")
     print("Uma mulher nesta casa")
-input3 = input("Input ")
+input3 = input("Resposta:")
 if input3 == "Joana":
     print("Correto!")
     print("Um homem nesta casa")
@@ -24,19 +24,59 @@ if input3 == "Joana":
 else:
     print("Errado!")
     print("Um homem nesta casa")
-input4 = input("Input ")
+input4 = input("Resposta:")
 if input4 == "Carlos":
     print("Correto!")
+    print("O nome de uma avó muito amada")
+    score = score+1
+else:
+    print("Errado!")
+    print("O Nome de uma avó muito amada")
+input6 = input("Resposta:")
+if input6 == "Avó Xana":
+    print("Correto!")
+    print("O nome de uma tia muito amada")
+    score = score+1
+else:
+    print("Errado!")
+    print("O nome de uma tia muito amada")
+input7 = input("Resposta:")
+if input7 == "Babá":
+    print("Correto!")
+    print("A idade de uma prima")
+    score = score+1
+else:
+    print("Errado!")
+    print("A idade de uma prima")
+input8 = input("Resposta:")
+if input8 == "4":
+    print("Correto!")
+    print("Uma comida muito popular")
+    score = score+1
+else:
+    print("Errado!")
+    print("Uma comida muito popular")
+input9 = input("Resposta:")
+if input9 == "Pizza":
+    print("Correto!")
+    print("O nome de uma prima muito amada")
+    score = score+1
+else:
+    print("Errado!")
+    print("O nome de uma prima muito amada")
+input10 = input("Resposta:")
+if input10 == "Ninha":
+    print("Correto!")
     print("O nome de uma pessoa muito amada")
     score = score+1
 else:
     print("Errado!")
     print("O nome de uma pessoa muito amada")
-input5 = input("Input ")
-if input5 == "João":
+input11 = input("Resposta:")
+if input11 == "João":
     print("Correto!")
     score = score+1
-    print("Your total score is ",score,"out of 10")
+    print("Tu tens",score,"respostas certas em 10 perguntas")
 else:
     print("Errado!")
-    print("Your total score is ",score,"out of 5")
+    print("Tu tens",score,"respostas certas em 10 perguntas")
