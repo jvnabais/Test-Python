b3fd8aba64264f7780f8ed84ed0e59c34ea3fabb
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-03-16 21:17:47 +0000

    Update

---
+++ JokesForMom.py
@@ -0,0 +1,22 @@
+corret_answer = "Correto"
+wrong_answer = "Errado"
+score = 0
+print("Uma coisa que o freddie faz muitas vezes?")
+input1 = input("Resposta: ")
+if input1 == "coco":
+    print("AAHAHAHAHAhaha")
+    print("Uma coisa muito comum ca em casa")
+    score = score+1
+else:
+    print("A RESPOSTA ERA COCO HAHAHA")
+    print("Uma coisa muito comum cá em casa")
+
+input2 = input("Resposta: ")
+if input2 == "pergiça":
+    print("ahahahahahaha")
+    input("HAHAHAHAHHAHA")
+    score = score+1
+else:
+    print("A RERPOSTA ERA PERGIÇA HAHAHAHAHA")
+    print("TU TENS", score, "RESPOSTAS CERTAS AHJAHAHAHA")
+    input("hahah bom trabalho <:")
